d1417995743308235c38528f5fbd99eb953fa17b
wangkuo2008 <wangxinmiao125@yahoo.com>
2020-05-13 20:47:32 +0900

    Parallelize with openMP

--- 02_openmp/14_bucket_sort.cpp
+++ 02_openmp/14_bucket_sort.cpp
@@ -6,20 +6,32 @@ int main() {
   int n = 50;
   int range = 5;
   std::vector<int> key(n);
-  for (int i=0; i<n; i++) {
-    key[i] = rand() % range;
-    printf("%d ",key[i]);
+  #pragma omp parallel
+  {
+    #pragma omp for
+    for (int i=0; i<n; i++) {
+      key[i] = rand() % range;
+      printf("%d ",key[i]);
+    }
   }
   printf("\n");
 
-  std::vector<int> bucket(range); 
-  for (int i=0; i<range; i++) {
-    bucket[i] = 0;
+  std::vector<int> bucket(range);
+  #pragma omp parallel
+  { 
+    #pragma omp for
+    for (int i=0; i<range; i++) {
+      bucket[i] = 0;
+    }
   }
+  #pragma omp parallel for
   for (int i=0; i<n; i++) {
+    #pragma omp atomic update
     bucket[key[i]]++;
   }
+#pragma omp paralle
   for (int i=0, j=0; i<range; i++) {
+    #pragma omp paralle for
     for (; bucket[i]>0; bucket[i]--) {
       key[j++] = i;
     }
